90ad24ec5aa46f6e83cb270a69ea76e48a3fb836
frank <frank@qlimix.net>
2018-11-11 13:58:56 +0100

    throw exception

--- src/ProcessControlInterface.php
+++ src/ProcessControlInterface.php
@@ -19,19 +19,38 @@ interface ProcessControlInterface
      * @param int $pid
      *
      * @return bool
+     *
+     * @throws ProcessException
      */
     public function isProcessRunning(int $pid): bool;
 
+    /**
+     * @param ProcessInterface $process
+     *
+     * @return int
+     *
+     * @throws ProcessException
+     */
     public function startProcess(ProcessInterface $process): int;
 
     /**
      * @param ProcessInterface[] $processes
      *
      * @return int
+     *
+     * @throws ProcessException
      */
     public function startProcesses(array $processes): int;
 
+    /**
+     * @param int $pid
+     *
+     * @throws ProcessException
+     */
     public function stopProcess(int $pid): void;
 
+    /**
+     * @throws ProcessException
+     */
     public function stopProcesses(): void;
 }
